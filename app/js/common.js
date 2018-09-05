486f66b8ce3c3ab06f0645b4e3dc135b6584dc0d
Ruslan Myznikov <ruslan.myznikov@nure.ua>
2018-09-05 18:02:58 +0300

    desktop view

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,34 @@
 $(function() {
 
-	// Custom JS
 
+});
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        760: {
+            items:2
+        },
+        1000:{
+            items:3
+        },
+        1300:{
+            items:4
+        }
+    }
+});
+
+var selector = $('.owl-carousel');
+
+$('.my-next-button').click(function() {
+    selector.trigger('next.owl.carousel');
+});
+
+$('.my-prev-button').click(function() {
+    selector.trigger('prev.owl.carousel');
 });
