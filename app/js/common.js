35f4ac6827184c0f4adba488af499c88d54c9ad2
Ruslan Myznikov <ruslan.myznikov@nure.ua>
2018-09-06 14:33:16 +0300

    adaptive added

--- app/js/common.js
+++ app/js/common.js
@@ -4,7 +4,7 @@ $(function() {
 });
 $('.owl-carousel').owlCarousel({
     loop:true,
-    margin:10,
+    margin:30,
     nav:false,
     dots: false,
     responsive:{
@@ -48,4 +48,36 @@ $(document).ready(function(){
         //анимируем переход на расстояние - top за 1500 мс
         $('body,html').animate({scrollTop: top}, 1500);
     });
+
+
+
 });
+$(document).ready(function(){
+    $("#menu-2").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+
+
+});
+// function scrollHeightTrigger (height, fn) {
+//     var old_height = $(document).scrollTop();
+//     $(document).scroll(function () {
+//         var new_height = $(document).scrollTop();
+//         if (((height < new_height) && (height >= old_height)) ||
+//             ((height < old_height) && (height >= new_height))) {
+//             fn();
+//         }
+//         old_height = new_height;
+//     });
+// }
